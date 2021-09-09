476380511b598ca2fb9d309f6a7336a06312da7f
DranKof <drankof@gmail.com>
2021-09-08 23:30:29 -0700

    DarrenS: Addition of script to build and run the docker container

---
+++ 02_node-scores_run.sh
@@ -0,0 +1,18 @@
+
+# Docker build and run script
+
+docker build ./node-scores/ -t node-scores
+
+docker stop node-scores
+docker container prune -f
+
+docker run \
+	--name "node-scores" \
+	--user "root" \
+	--restart "unless-stopped" \
+	--volume "/path/to/data/node-scores:/usr/src/app/data" \
+	--volume "/path/to/logs/node-scores:/usr/src/app/logs" \
+	--detach \
+	node-scores
+
+#	--publish "3300:3300" \
